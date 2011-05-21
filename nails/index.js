f7621578eb172b86611b21ce1857f5af487e0afc
Nick Radford <nick@Nick-Radfords-MacBook-Pro.local>
2011-05-21 00:14:15 -0500

    ports

--- nails/index.js
+++ nails/index.js
@@ -1,4 +1,8 @@
 nails = require("./nails");
 
-
-nails.net.server.start(9001);
+console.log(process.argv);
+if (process.argv.length = 2)
+	port = 9001;
+else
+	port = parseInt(process.argv[3]);
+nails.net.server.start(port);
